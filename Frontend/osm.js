d51b811b5aa0c81b92df60b3fff0a643e86722d0
Potatoish <ambulariopearl@gmail.com>
2026-04-05 20:58:14 +0800

    .

--- Frontend/osm.js
+++ Frontend/osm.js
@@ -1,4 +1,4 @@
-const USE_OSRM = true; //optional toggle for OSRM routing - set to false to use simple straight lines between nodes
+const USE_OSRM = false; //optional toggle for OSRM routing - set to false to use simple straight lines between nodes
 const OSRM_BASE_URL = 'https://router.project-osrm.org';
 const ROUTE_OFFSETS = [0, 5, -5, 10, -10];
 
