0f54b3f8cc28c5e7c9aa271214b8728964b9cb6b
Potatoish <ambulariopearl@gmail.com>
2026-04-10 06:04:34 +0800

    .

--- Backend/data/database.py
+++ Backend/data/database.py
@@ -2,8 +2,8 @@ import pyodbc
 
 CONNECTION_STRING = (
     "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=DESKTOP-THN5GFN\\SQLEXPRESS;" 
-    "DATABASE=DIsaster_route_simulation;"
+    "SERVER=PRLY04\\SQLEXPRESS;" 
+    "DATABASE=disaster_route_sim;"
     "Trusted_Connection=yes;"
     "TrustServerCertificate=yes;"
     #SQL
