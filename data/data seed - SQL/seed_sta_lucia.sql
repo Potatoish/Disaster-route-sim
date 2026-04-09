87443dbd962a11bddbc3bb31d623ab46bdcd3623
blckjn <jessaanunezz@gmail.com>
2026-04-09 11:35:06 +0800

    .

--- data/data seed - SQL/seed_sta_lucia.sql
+++ data/data seed - SQL/seed_sta_lucia.sql
@@ -5,12 +5,12 @@ GO
 INSERT INTO nodes (name, lat, lng, barangay, node_type)
 SELECT v.name, v.lat, v.lng, v.barangay, v.node_type
 FROM (VALUES
-    ('Sta. Lucia Barangay Hall',           14.58826428, 121.09991886, 'Sta. Lucia', 'landmark'),
-    ('De Castro Elementary School',        14.58752774, 121.09887019, 'Sta. Lucia', 'evacuation_center'),
-    ('Sta. Lucia Bliss Multipurpose Hall', 14.57871059, 121.09861182, 'Sta. Lucia', 'evacuation_center'),
-    ('Santa Lucia High School (Tramo St.)',14.58303503, 121.10359729, 'Sta. Lucia', 'landmark'),
-    ('Brgy. Sta. Lucia Health Center',     14.58497931, 121.10165443, 'Sta. Lucia', 'health_facility'),
-    ('East Ortigas Mansions',              14.58633218, 121.10351134, 'Sta. Lucia', 'start_node')
+    ('Sta. Lucia Barangay Hall',           14.588305813024784, 121.10059477816179, 'Sta. Lucia', 'landmark'),
+    ('De Castro Elementary School',        14.587299317821408, 121.09888701920387, 'Sta. Lucia', 'evacuation_center'),
+    ('Sta. Lucia Bliss Multipurpose Hall', 14.579498397619826, 121.10277475864685, 'Sta. Lucia', 'evacuation_center'),
+    ('Santa Lucia High School (Tramo St.)',14.583107716145452, 121.10404790145412, 'Sta. Lucia', 'landmark'),
+    ('Brgy. Sta. Lucia Health Center',     14.584959232442822, 121.10178292289898, 'Sta. Lucia', 'health_facility'),
+    ('East Ortigas Mansions',              14.58632175410495, 121.10358111094102, 'Sta. Lucia', 'start_node')
 ) AS v(name, lat, lng, barangay, node_type)
 WHERE NOT EXISTS (
     SELECT 1
